bf96fbf48f340ec55182eb9da967dc0eadb2a9e3
BoschiAlbano <iprogramacion17@gmail.com>
2024-02-22 13:13:45 -0300

    Desarrollo Variables Entorno

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 // Variables Publicas (cliente) que no funcionan en azure app serive
-// azure: https://distrinova.azurewebsites.net
-// local: http://localhost:3000
-
+// Azure Produccion: https://distrinova.azurewebsites.net
+// Local: http://localhost:3000
+// Azure Desarrollo: https://distrinova-desarrollo.azurewebsites.net
 const Publicas = {
     // NEXT_PUBLIC_HOST: 'https://distrinova-martin.azurewebsites.net',
-    NEXT_PUBLIC_HOST: "http://localhost:3000",
+    //NEXT_PUBLIC_HOST: "http://localhost:3000",
+    NEXT_PUBLIC_HOST: "https://distrinova-desarrollo.azurewebsites.net",
 };
 
 export default Publicas;
